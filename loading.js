914453faf180977edfa5b1fd4e750f3689a3c5df
Gabriel Salazar <133398841+GaboscaNullcode@users.noreply.github.com>
2023-09-21 11:22:42 -0500

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,58 @@
+// Create the overlay
+const overlay = document.createElement('div');
+overlay.id = 'loader';
+overlay.style.cssText = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 999;
+`;
+
+// Create the loader using a div element
+const loader = document.createElement('div');
+loader.style.cssText = `
+    border: 6px solid #f3f3f3;
+    border-top: 6px solid #000;
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    animation: spin 2s linear infinite;
+`;
+
+// Append the loader to the overlay
+overlay.appendChild(loader);
+
+// Append the overlay to the body
+document.body.appendChild(overlay);
+
+// Create a style element for the loader animation
+const style = document.createElement('style');
+style.innerHTML = `
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+`;
+
+// Append the style to the head of the document
+document.head.appendChild(style);
+
+// Function to hide the overlay
+function hideOverlay() {
+    overlay.style.display = 'none';
+}
+
+// Add an event listener to hide the overlay when all external JS files are loaded
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(hideOverlay, 100); // Simulate loading external JS files.
+});
+
+// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
+// we'll still hide the overlay when the window's load event is triggered.
+window.addEventListener('load', hideOverlay);
